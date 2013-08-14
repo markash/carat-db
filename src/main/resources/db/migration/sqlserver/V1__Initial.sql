7af05e225af962ad677aa7a4d04ddeff7fe3ffb9
markash <mp.ashworth@gmail.com>
2013-08-15 00:03:35 +0200

    Added SQL Server support

--- src/main/resources/db/migration/sqlserver/V1__Initial.sql
+++ src/main/resources/db/migration/sqlserver/V1__Initial.sql
@@ -1,18 +1,18 @@
-CREATE TABLE item (
+CREATE TABLE base.item (
   id INT IDENTITY PRIMARY KEY,
   description VARCHAR(100),
   created DATETIME NULL,
   updated DATETIME NULL
 );
 
-INSERT INTO item (description) VALUES ('Test1');
-INSERT INTO item (description) VALUES ('Test2');
-INSERT INTO item (description) VALUES ('Test3');
-INSERT INTO item (description) VALUES ('Test4');
-INSERT INTO item (description) VALUES ('Test5');
-INSERT INTO item (description) VALUES ('Test6');
+INSERT INTO base.item (description) VALUES ('Test1');
+INSERT INTO base.item (description) VALUES ('Test2');
+INSERT INTO base.item (description) VALUES ('Test3');
+INSERT INTO base.item (description) VALUES ('Test4');
+INSERT INTO base.item (description) VALUES ('Test5');
+INSERT INTO base.item (description) VALUES ('Test6');
 
-CREATE TABLE app_user (
+CREATE TABLE base.app_user (
   id INT IDENTITY PRIMARY KEY,
   password VARCHAR(MAX) NOT NULL,
   username VARCHAR(32) NOT NULL,
@@ -22,11 +22,11 @@ CREATE TABLE app_user (
   UNIQUE(username)
 );
 
-CREATE TABLE app_role (
+CREATE TABLE base.app_role (
   role VARCHAR(32) UNIQUE
 );
 
-CREATE TABLE app_user_role (
+CREATE TABLE base.app_user_role (
   user_id INTEGER NOT NULL,
   user_role VARCHAR(32),
   UNIQUE(user_id, user_role),
@@ -34,9 +34,9 @@ CREATE TABLE app_user_role (
   CONSTRAINT fk_user_role_role FOREIGN KEY (user_role) REFERENCES app_role(role)
 );
 
-INSERT INTO app_role (role) VALUES ('EMPLOYEE');
-INSERT INTO app_role (role) VALUES ('MANAGER');
-INSERT INTO app_role (role) VALUES ('ADMIN');
+INSERT INTO base.app_role (role) VALUES ('EMPLOYEE');
+INSERT INTO base.app_role (role) VALUES ('MANAGER');
+INSERT INTO base.app_role (role) VALUES ('ADMIN');
 
-INSERT INTO app_user (username, password, email, first_name, last_name) VALUES ('admin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'admin@localhost', 'Administrator', 'User');
-INSERT INTO app_user_role (user_id, user_role) values (SCOPE_IDENTITY(), 'ADMIN');
+INSERT INTO base.app_user (username, password, email, first_name, last_name) VALUES ('admin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'admin@localhost', 'Administrator', 'User');
+INSERT INTO base.app_user_role (user_id, user_role) values (SCOPE_IDENTITY(), 'ADMIN');
